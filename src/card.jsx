6b9ffcdf05c8d54fe8e160496526552250ff9c17
Dario <ferrada86@gmail.com>
2021-02-12 01:11:39 +0100

    bb

--- src/card.jsx
+++ src/card.jsx
@@ -1,25 +1,22 @@
 import '../node_modules/materialize-css/dist/css/materialize.min.css';
 import React from "react";
-import { Link } from 'react-router-dom';
 
-const Card = ({ id, image, title, description})=> (   
+const Card = ({key, id, image, title, descrption})=> (
+    </div>    
     <div className="col s3">
-        <div>
-            <div className="card">
-                <div className="card-image">
-                    <Link to={`/viajes/${id}`}>
-                        <img src={image} />   
-                    </Link>
-                    <span className="card-title">{title}</span>
-                </div>
-                <div className="card-content">
-                    <p>{description}</p>
-                </div>
-                <div className="card-action">
-                    <Link to={`/viajes/${id}`}>This is a link</Link>
-                </div>
-            </div>
-        </div>
-    </div>
+         <div>
+             <div className="card">
+                 <div className="card-image">
+                     <img src={image} />
+                     <span className="card-title">{title}</span>
+                 </div>
+                 <div className="card-content">
+                     <p>{description}</p>
+                 </div>
+                 <div className="card-action">
+                     <a href="#">This is a link</a>
+                 </div>
+             </div>
+         </div>
+     </div>
 )
-export default Card
